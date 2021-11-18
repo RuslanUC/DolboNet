8164ac2492ced5cf89712e08731547bcd5279265
RuslanUC <dev_ruslan_uc@protonmail.com>
2021-11-18 19:00:30 +0200

    replace CheckingClient with check_token

--- bot.py
+++ bot.py
@@ -3,29 +3,25 @@
 # https://github.com/wokashi-rg
 
 from nextcord import Intents
-from nextcord.errors import LoginFailure
-from core.checking_client import CheckingClient
+from core.check_token import check_token
 import config
 from utils.tprint import log
 import asyncio
 
 log("Проверяю Discord токен...")
-checking_client = CheckingClient(intents=Intents.none())
-login_successful = False
-try:
-    checking_client.run(config.token)
-    login_successful = True
-except LoginFailure:
+
+if not check_token(config.token):
     log("НЕВЕРНЫЙ DISCORD ТОКЕН! Необходимо отредактировать файл config.py!")
+    import sys
+    sys.exit()
 
-if login_successful:
-    log("Discord токен проверен.")
-    asyncio.set_event_loop(asyncio.new_event_loop())
-    from core.main_client import MainClient
+log("Discord токен проверен.")
+asyncio.set_event_loop(asyncio.new_event_loop())
+from core.main_client import MainClient
 
-    intents = Intents.none()
-    intents.guilds = True
-    intents.guild_messages = True
-    intents.emojis = True
-    main_client = MainClient(intents=intents)
-    main_client.run(config.token)
+intents = Intents.none()
+intents.guilds = True
+intents.guild_messages = True
+intents.emojis = True
+main_client = MainClient(intents=intents)
+main_client.run(config.token)
